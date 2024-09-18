abb4e776ad030733569df48cc7c4cf18f72cb223
Braian Mellor <braianj@gmail.com>
2024-09-18 17:42:54 -0300

    refactor: deployEntity

--- src/adapters/deployer/index.ts
+++ src/adapters/deployer/index.ts
@@ -19,67 +19,70 @@ export function createDeployerComponent(
       try {
         const exists = await components.storage.exist(entity.entityId)
 
+        const isSnsEntityToSend =
+          (entity.entityType === 'scene' || entity.entityType === 'wearable' || entity.entityType === 'emote') &&
+          !!components.sns.arn
+
+        const isSnsEventToSend = !!components.sns.eventArn
+
+        if (exists) {
+          return await markAsDeployed()
+        }
+
         const deploymentToSqs: DeploymentToSqs = {
           entity,
           contentServerUrls: servers
         }
 
-        if (!exists) {
-          if (
-            (entity.entityType === 'scene' || entity.entityType === 'wearable' || entity.entityType === 'emote') &&
-            components.sns.arn
-          ) {
-            await components.downloadQueue.onSizeLessThan(1000)
+        if (isSnsEntityToSend) {
+          await components.downloadQueue.onSizeLessThan(1000)
 
-            void components.downloadQueue.scheduleJob(async () => {
-              logger.info('Downloading entity', {
-                entityId: entity.entityId,
-                entityType: entity.entityType,
-                servers: servers.join(',')
-              })
-
-              await downloadEntityAndContentFiles(
-                { ...components, fetcher: components.fetch },
-                entity.entityId,
-                servers,
-                new Map(),
-                'content',
-                10,
-                1000
-              )
-
-              logger.info('Entity stored', { entityId: entity.entityId, entityType: entity.entityType })
-              // send sns
-              const receipt = await client.send(
-                new PublishCommand({
-                  TopicArn: components.sns.arn,
-                  Message: JSON.stringify(deploymentToSqs)
-                })
-              )
-              logger.info('Notification sent', {
-                MessageId: receipt.MessageId as any,
-                SequenceNumber: receipt.SequenceNumber as any
-              })
+          void components.downloadQueue.scheduleJob(async () => {
+            logger.info('Downloading entity', {
+              entityId: entity.entityId,
+              entityType: entity.entityType,
+              servers: servers.join(',')
             })
-          }
 
-          if (components.sns.eventArn) {
+            await downloadEntityAndContentFiles(
+              { ...components, fetcher: components.fetch },
+              entity.entityId,
+              servers,
+              new Map(),
+              'content',
+              10,
+              1000
+            )
+
+            logger.info('Entity stored', { entityId: entity.entityId, entityType: entity.entityType })
+            // send sns
             const receipt = await client.send(
               new PublishCommand({
-                TopicArn: components.sns.eventArn,
+                TopicArn: components.sns.arn,
                 Message: JSON.stringify(deploymentToSqs)
               })
             )
-            logger.info('Notification sent to events SNS', {
+            logger.info('Notification sent', {
               MessageId: receipt.MessageId as any,
               SequenceNumber: receipt.SequenceNumber as any
             })
-          }
-
-          await markAsDeployed()
-        } else {
-          await markAsDeployed()
+          })
         }
+
+        if (isSnsEventToSend) {
+          const receipt = await client.send(
+            new PublishCommand({
+              TopicArn: components.sns.eventArn,
+              Message: JSON.stringify(deploymentToSqs)
+            })
+          )
+          logger.info('Notification sent to events SNS', {
+            MessageId: receipt.MessageId as any,
+            SequenceNumber: receipt.SequenceNumber as any
+          })
+        }
+
+        await markAsDeployed()
       } catch (error: any) {
         const isNotRetryable = /status: 4\d{2}/.test(error.message)
         if (isNotRetryable) {
