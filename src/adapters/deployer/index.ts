61988ae1c961c8e3b6fc8cee47e548a4a6151dc3
Manuel Ponsa <manuponsa@gmail.com>
2024-09-18 17:24:12 -0300

    fix: don't save profiles in s3 bucket

--- src/adapters/deployer/index.ts
+++ src/adapters/deployer/index.ts
@@ -19,50 +19,51 @@ export function createDeployerComponent(
       try {
         const exists = await components.storage.exist(entity.entityId)
 
+        const deploymentToSqs: DeploymentToSqs = {
+          entity,
+          contentServerUrls: servers
+        }
+
         if (!exists) {
-          await components.downloadQueue.onSizeLessThan(1000)
+          if (
+            (entity.entityType === 'scene' || entity.entityType === 'wearable' || entity.entityType === 'emote') &&
+            components.sns.arn
+          ) {
+            await components.downloadQueue.onSizeLessThan(1000)
 
-          void components.downloadQueue.scheduleJob(async () => {
-            logger.info('Downloading entity', {
-              entityId: entity.entityId,
-              entityType: entity.entityType,
-              servers: servers.join(',')
-            })
+            void components.downloadQueue.scheduleJob(async () => {
+              logger.info('Downloading entity', {
+                entityId: entity.entityId,
+                entityType: entity.entityType,
+                servers: servers.join(',')
+              })
 
-            await downloadEntityAndContentFiles(
-              { ...components, fetcher: components.fetch },
-              entity.entityId,
-              servers,
-              new Map(),
-              'content',
-              10,
-              1000
-            )
-
-            logger.info('Entity stored', { entityId: entity.entityId, entityType: entity.entityType })
-
-            const deploymentToSqs: DeploymentToSqs = {
-              entity,
-              contentServerUrls: servers
-            }
-            // send sns
-            if (
-              (entity.entityType === 'scene' || entity.entityType === 'wearable' || entity.entityType === 'emote') &&
-              components.sns.arn
-            ) {
-              const receipt = await client.send(
-                new PublishCommand({
-                  TopicArn: components.sns.arn,
-                  Message: JSON.stringify(deploymentToSqs)
-                })
+              await downloadEntityAndContentFiles(
+                { ...components, fetcher: components.fetch },
+                entity.entityId,
+                servers,
+                new Map(),
+                'content',
+                10,
+                1000
               )
-              logger.info('Notification sent', {
-                MessageId: receipt.MessageId as any,
-                SequenceNumber: receipt.SequenceNumber as any
+
+              logger.info('Entity stored', { entityId: entity.entityId, entityType: entity.entityType })
+              // send sns
+                const receipt = await client.send(
+                  new PublishCommand({
+                    TopicArn: components.sns.arn,
+                    Message: JSON.stringify(deploymentToSqs)
+                  })
+                )
+                logger.info('Notification sent', {
+                  MessageId: receipt.MessageId as any,
+                  SequenceNumber: receipt.SequenceNumber as any
+                })
               })
             }
 
-            if (components.sns.eventArn) {
+          if (components.sns.eventArn) {
               const receipt = await client.send(
                 new PublishCommand({
                   TopicArn: components.sns.eventArn,
@@ -73,9 +74,11 @@ export function createDeployerComponent(
                 MessageId: receipt.MessageId as any,
                 SequenceNumber: receipt.SequenceNumber as any
               })
-            }
-            await markAsDeployed()
-          })
+          }
+            
+          
+          await markAsDeployed()
+          
         } else {
           await markAsDeployed()
         }
